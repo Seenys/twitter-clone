1b7aea004fa906e0087c493af035d7923e12074e
Step <stp.sip7@gmail.com>
2023-03-21 17:21:25 -0500

    end sidebar styling & components

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,9 +1,56 @@
+// React
 import React from "react";
+// Components
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+// Icons
+import { FaUser } from "react-icons/fa";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
+import SidebarTweetButton from "./SidebarTweetButton";
 
-type Props = {};
-
-const Sidebar = (props: Props) => {
-  return <div>Sidebar test</div>;
+const items = [
+  {
+    label: "Home",
+    href: "/",
+    icon: BsHouseFill,
+  },
+  {
+    label: "Notifications",
+    href: "/notifications",
+    icon: BsBellFill,
+  },
+  {
+    label: "Profile",
+    href: "/user/123",
+    icon: FaUser,
+  },
+];
+const Sidebar = () => {
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              label={item.label}
+              href={item.href}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem
+            onClick={() => {}}
+            label="Logout"
+            href="/logout"
+            icon={BiLogOut}
+          />
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
