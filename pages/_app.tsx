5f5fa723b2c1ad99c012f6693a76c8995d1da6b8
Step <stp.sip7@gmail.com>
2023-04-05 17:10:31 -0500

    hooks & zustand

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 //Components
 import Layout from "@/components/Layout";
-import Modal from "@/components/shared/Modal";
 //Styles
 import "@/styles/globals.css";
 
