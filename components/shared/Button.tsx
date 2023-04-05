e5c16113d5b41d78e5ee11a6e4f58cfb333583de
Step <stp.sip7@gmail.com>
2023-04-05 16:05:17 -0500

    modal and button ready

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -20,7 +20,27 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
 }) => {
   return (
-    <button className=" " onClick={onClick}>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`
+      disabled:opacity-70
+      disabled:cursor-not-allowed
+      rounded-full
+      font-semibold
+      hover:opacity-80
+      transition
+      border-2
+      ${fullWidth ? "w-full" : "w-fit"}
+      ${large ? "py-3 px-5 text-xl" : "py-2 px-4 text-md"}
+      ${
+        secondary
+          ? "bg-white text-black border-black"
+          : "bg-sky-500 text-white border-sky-500"
+      }
+      ${outline ? "bg-transparent text-white border-white" : ""}
+      `}
+    >
       {label}
     </button>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from "next/app";
 //Components
 import Layout from "@/components/Layout";
+import Modal from "@/components/shared/Modal";
 //Styles
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
