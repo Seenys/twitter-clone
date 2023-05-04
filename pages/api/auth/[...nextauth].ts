3cb1e1c9e7a86d16a60ca9dfd145dabefd078a4c
Step <stp.sip7@gmail.com>
2023-05-03 19:38:41 -0500

    resgister api, prima config, nexthauth config

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,56 @@
+// next-auth imports
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+// adapters
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// other imports
+import bcrypt from "bcrypt";
+
+// libs
+import { prisma } from "@/libs/";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXT_AUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+});
