6f66a23984f755ee6dd18fda48de37fef4115bb9
Step <stp.sip7@gmail.com>
2023-05-05 19:54:43 -0500

    follow bars ands random avatars

--- components/shared/Avatar.tsx
+++ components/shared/Avatar.tsx
@@ -1,13 +1,61 @@
-import React from "react";
+// React Imports
+import React, { useCallback } from "react";
+// Next
+import Image from "next/image";
+// Hooks
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/router";
+// Others
+import Avatar from "boring-avatars";
 
 type avatarProps = {
   userId: string;
   isLarge?: boolean;
-  isSmall?: boolean;
+  hasBorder?: boolean;
 };
 
-const Avatar: React.FC<avatarProps> = ({ userId, isLarge, isSmall }) => {
-  return <div>Avatar</div>;
+const MainAvatar: React.FC<avatarProps> = ({ userId, isLarge, hasBorder }) => {
+  const router = useRouter();
+  const { data: fetchedUser } = useUser(userId);
+
+  const onClick = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+      console.log("Avatar Clicked");
+
+      const url = `/users/${userId}`;
+      router.push(url);
+    },
+    [router, userId]
+  );
+
+  return (
+    <div
+      className={`${hasBorder ? "border-4 border-black" : ""} ${
+        isLarge ? "w-32 h-32" : "w-12 h-12"
+      } rounded-full cursor-pointer hover:opacity-90 transition relative`}
+    >
+      {fetchedUser?.profileImage ? (
+        <Image
+          fill
+          style={{
+            objectFit: "cover",
+            borderRadius: "100%",
+          }}
+          alt="Avatar"
+          src={fetchedUser?.profileImage}
+          onClick={onClick}
+        />
+      ) : (
+        <Avatar
+          size={40}
+          name={fetchedUser?.name || "John Doe"}
+          variant="beam"
+          colors={["#AFFBFF", "#D2FDFE", "#FEFAC2", "#FEBF97", "#FE6960"]}
+        />
+      )}
+    </div>
+  );
 };
 
-export default Avatar;
+export default MainAvatar;
